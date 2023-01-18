b35bfcf6189ec563a5fd6dd02e1e71a321cffb90
greatamani <amani.ephraim@yahoo.com>
2023-01-18 08:37:46 -0800

    Print string in reverse order

---
+++ 0x08-recursion/1-puts_recursion.c
@@ -0,0 +1,17 @@
+ #include <stdio.h>
+ #include "main.h"
+
+ /**
+  * _print_rev_recursion - prints a string in reverse, followed by a new line
+  * @s: string to print
+  *
+  **/
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
