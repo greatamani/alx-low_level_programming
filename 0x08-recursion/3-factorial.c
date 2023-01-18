1127c5b1d4b2d194ffb01ee1c334acfc36c513f9
greatamani <amani.ephraim@yahoo.com>
2023-01-18 10:25:55 -0800

    Factorial of a given number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * factorial - Calculate the actorial of a given number
+ * @n: Given number
+ * Return: The factorial of a given number
+ **/
+
+int factorial(int n)
+{
+	if (n < 0)
+	return (-1);
+
+	return (n * factorial(n - 1));
+}
