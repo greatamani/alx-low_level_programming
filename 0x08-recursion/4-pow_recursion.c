c45f8e0338f5c0026db05d172a8a12061a584ada
greatamani <amani.ephraim@yahoo.com>
2023-01-18 11:06:25 -0800

    X raised to power Y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - Cvalue of x raised to the power of y
+ * @x: Given number
+ * @y: raised power
+ * Return: The power of x raised to y
+ **/
+
+int _pow_recursion(int x, int y)
+{
+	int i = x;
+	int j = y;
+
+	if (j < 0)
+	{
+		return (0);
+	} else if (j == 1)
+	{
+		return (i);
+	}
+
+	return (i * _pow_recursion(i, j - 1));
+}
