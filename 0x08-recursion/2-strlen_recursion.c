8e665000b45b49570d4d37b0466bfc3dab2d842b
greatamani <amani.ephraim@yahoo.com>
2023-01-18 10:07:34 -0800

    Function to return the length of string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -12,11 +12,9 @@ int _strlen_recursion(char *s)
 
 	if (*s == '\0')
 	{
-		 len = len + 1;
-
-		_strlen_recursion(s + 1);
+		return (0);
 	}
 
-	return (len);
+	return (1 + _strlen_recursion(s + 1));
 
 }
