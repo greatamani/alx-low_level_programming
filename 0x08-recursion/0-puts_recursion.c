b98f2b8731f8425ff750d65bd597a74d1ea99723
greatamani <amani.ephraim@yahoo.com>
2023-01-18 08:00:07 -0800

    Recursion, Print string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,8 +2,7 @@
 #include "main.h"
 
 /**
- * 
- *  _puts_recursion - prints a string, followed by a new line
+ * _puts_recursion - prints a string, followed by a new line
  * @s: string to print
  *
  **/
