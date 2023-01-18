b401df2e8994c583f330f4bc631f2b5fef5081c1
greatamani <amani.ephraim@yahoo.com>
2023-01-18 08:43:02 -0800

    Print string in reverse order

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,7 +2,7 @@
  #include "main.h"
 
  /**
-  * _print_rev_recursion - prints a string in reverse, followed by a new line
+  * _print_rev_recursion - prints a string in reverse
   * @s: string to print
   *
   **/
