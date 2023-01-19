13d50e490a6f6262c9334227b9a9aa027cb4a9bb
greatamani <amani.ephraim@yahoo.com>
2023-01-19 08:52:45 -0800

    prints the number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: Number of arguments passed to main
+ * @argv: Array of strings of arguments passed to main function
+ * Return: exit Always EXIT_SUCCESS
+ *
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%d\n", argc);
+
+	exit(EXIT_SUCCESS);
+}
