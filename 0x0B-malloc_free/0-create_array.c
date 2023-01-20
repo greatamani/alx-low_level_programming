460664f05322fe6d8c6eee333f48677417e387d8
greatamani <amani.ephraim@yahoo.com>
2023-01-20 05:12:29 -0800

    Malloc/free - reates an array of chars

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include <stdio.c>
+#include "main.h"
+
+/**
+ * create_array - an array of chars, and initializes it with a specific char
+ * @size: array size argument
+ * @c: character passed to function
+ *
+ * Return: pointer to an array *
+ *
+ **/
+
+char *create_array(unsigned int size, char c)
+{
+	char *a;
+
+	a = malloc(s * sizeof(char));
+	
+	if( size == 0 || a == NULL)
+	{
+		free(a);
+		return (NULL);
+	}
+
+	while (size--)
+		*(a + size) = c;
+
+	return (a);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,2 @@
+MALLOC & FREE memory usage control
+0. a function that creates an array of chars, and initializes it with a specific char
