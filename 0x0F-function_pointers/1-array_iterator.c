22f2ab10896f5aa822d126678230e970e7411c5b
greatamani <amani.ephraim@yahoo.com>
2023-01-25 02:48:19 -0800

    Array iteration

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -16,11 +16,8 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	unsigned int i;
 
 	if (!array || !action)
-		return ();
+		return;
 
 	for (i = 0; i < size; i++)
-	{
 		action(array[i]);
-
-	}
 }
