58a48b020a145643760f22e844ca93cbb3963f89
greatamani <amani.ephraim@yahoo.com>
2023-01-27 07:37:13 -0800

    Adds all arguments passed

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all  - Function to sum variadic arguments
+ * @n: Constant size of arguments
+ *
+ * Return: Sum of arguments
+ *
+ **/
+
+nt sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+
+	va_start(ap, n);
+
+	if (n == 0)
+		return (0);
+
+	int i;
+	int sum;
+
+	sum = 0;
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, int);
+
+	va_end(ap);
+
+	return (sum);
+}
