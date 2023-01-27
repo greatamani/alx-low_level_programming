bad649cbe9048cc63f854913964ff65a2f39145b
greatamani <amani.ephraim@yahoo.com>
2023-01-27 07:52:07 -0800

    Adds all arguments passed

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,14 +13,13 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list ap;
+	unsigned int i;
+	int sum;
 
 	va_start(ap, n);
 
 	if (n == 0)
 		return (0);
-
-	int i;
-	int sum;
 
 	sum = 0;
 
