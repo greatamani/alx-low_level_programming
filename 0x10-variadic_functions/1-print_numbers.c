d34dd17a0433c2e06c90e9afb63e469e7f478790
greatamani <amani.ephraim@yahoo.com>
2023-01-28 06:32:41 -0800

    Print Numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,13 +19,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(va, n);
 
-	if (separator == NULL)
-		exit(EXIT_SUCCESS);
-
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(va, int));
-		if (i < (n - 1))
+		if (separator && i < (n - 1))
 		printf("%s", separator);
 	}
 
