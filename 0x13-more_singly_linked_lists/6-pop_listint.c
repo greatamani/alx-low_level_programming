47bab7d841ecd509f0ec45b3fbcd3072f6da62fc
greatamani <amani.ephraim@yahoo.com>
2023-02-15 23:39:59 -0800

    pop from list

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -15,13 +15,14 @@ int pop_listint(listint_t **head)
 {
 	listint_t *ptr;
 	int data = 0;
+
 	ptr = malloc(sizeof(listint_t));
 
 	if (head == NULL) /* account for no linked list */
 		return (0);
 
 	if (*head != NULL)
-	{	
+	{
 		ptr = *head;
 		data = ptr->n;
 		*head = ptr->next;
