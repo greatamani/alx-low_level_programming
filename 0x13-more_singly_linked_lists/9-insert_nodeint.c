bd0b6cb02a623e69576177ba16c05dcf47a23d8d
greatamani <amani.ephraim@yahoo.com>
2023-02-16 10:48:05 -0800

    Insert node in-between

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -38,7 +38,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	temp = *head;
 
-	for (nodex = 0; nodex < idx; nodex++)
+	for (nodex = 0; nodex < (idx - 1); nodex++)
 	{
 		if (temp->next != NULL)
 		{
