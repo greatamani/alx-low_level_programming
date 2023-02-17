abcdee5220a98ebf008db800f1b148ea51905d27
greatamani <amani.ephraim@yahoo.com>
2023-02-17 02:00:51 -0800

    Delete node

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -18,7 +18,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *tmp;
 
 	/* account for empty pointer */
-	if (head == NULL)
+	if (*head == NULL)
 		return (-1);
 
 	/* account for index 0 */
