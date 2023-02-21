3ffce12f691158afe55f5996aadbda30d83c4dc1
greatamani <amani.ephraim@yahoo.com>
2023-02-21 03:58:46 -0800

    Binary to U-Int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,51 +1,55 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * convertStringToInteger - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
+ */
+
 unsigned int convertStringToInteger(char *s)
 {
 	unsigned int i, d, n, len, f, digit;
- 
-          i = 0;
-          d = 0;
-          n = 0;
-          len = 0;
-          f = 0;
-          digit = 0;
- 
-          while (s[len] != '\0')
-	  {
-		 if (s[len] < '0' || s[len] > '1')
-			 return (n);
-		  len++;
-	  }
- 
-          while (i < len && f == 0)
-          {
-                  if (s[i] == '-')
-                  	++d;
-		  
-                  if (s[i] == '0' || s[i] == '1')
-                  {
-                          digit = s[i] - '0';
-                          if (d % 2)
-                                  digit = -digit;
-                          n = n * 10 + digit;
 
-                          f = 1;
-                          if (s[i + 1] < '0' || s[i + 1] > '9')
-			  {
-				  break;
-			  }
-                          f = 0;
-                  } 
-                  i++;
-          }
+	i = d = n = len = f = digit = 0;
 
-         if (f == 0)
-                  return (0);
- 
-          return (n);
+	while (s[len] != '\0')
+	{
+		if (s[len] < '0' || s[len] > '1')
+			return (n);
+		len++;
+	}
+
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] == '0' || s[i] == '1')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+
+			n = n * 10 + digit;
+
+			f = 1;
+
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+
+		i++;
+	}
+
+	if (f == 0)
+		return (0);
+
+	return (n);
 }
+
 /**
  * binary_to_uint - function that converts a binary number to an unsigned int
  * @b: attribute consist of binary variable
@@ -62,7 +66,10 @@ unsigned int binary_to_uint(const char *b)
 
 	/* Convert string to integer*/
 
-	str =(char *)b;
+	str = (char *)b;
+
+	if (str == NULL)
+		return (0);
 
 	num = convertStringToInteger(str);
 
@@ -79,7 +86,7 @@ unsigned int binary_to_uint(const char *b)
 		num = num / 10; /* divide the number with quotient*/
 		base = base * 2;
 	}
-	
+
 	return (decimal_num);
 
 }
