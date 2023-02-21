30ae1af874919732d6b766c7cc4eb6ec5d49c495
greatamani <amani.ephraim@yahoo.com>
2023-02-21 12:36:45 -0800

    Binary to U-Int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,55 +2,6 @@
 #include "main.h"
 
 /**
- * convertStringToInteger - converts a string to an integer
- * @s: string to be converted
- *
- * Return: the int converted from the string
- */
-
-unsigned int convertStringToInteger(char *s)
-{
-	unsigned int i, d, n, len, f, digit;
-
-	i = d = n = len = f = digit = 0;
-
-	while (s[len] != '\0')
-	{
-		if (s[len] < '0' || s[len] > '1')
-			return (n);
-		len++;
-	}
-
-	while (i < len && f == 0)
-	{
-		if (s[i] == '-')
-			++d;
-
-		if (s[i] == '0' || s[i] == '1')
-		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-
-			n = n * 10 + digit;
-
-			f = 1;
-
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
-		}
-
-		i++;
-	}
-
-	if (f == 0)
-		return (0);
-
-	return (n);
-}
-
-/**
  * binary_to_uint - function that converts a binary number to an unsigned int
  * @b: attribute consist of binary variable
  *
@@ -60,33 +11,28 @@ unsigned int convertStringToInteger(char *s)
 
 unsigned int binary_to_uint(const char *b)
 {
-	char *str;
 
-	unsigned int num, decimal_num = 0, base = 1, rem;
+	int len = 0, pow = 1;
+	unsigned int sum = 0, error = 0;
 
-	/* Convert string to integer*/
+	if (b == NULL)
+		return (error);
 
-	str = (char *)b;
+	while (b[len] != '\0') // find string length
+		len++;
+	len -= 1;
 
-	if (str == NULL)
-		return (0);
-
-	num = convertStringToInteger(str);
-
-	 /* assign the binary number to the binary_num variable*/
-
-	while (num > 0)
+	while (len >= 0) // iterate from back of string
 	{
-		rem = num % 10; /**
-				* divide the binary number by 10 and
-				* store the remainder in rem variable.
-				**/
-		decimal_num = decimal_num + rem * base;
+		if ((b[len] != '0') && (b[len] != '1'))
+			return (error);
 
-		num = num / 10; /* divide the number with quotient*/
-		base = base * 2;
+		if (b[len] == '1') // add appropriate power of 2 if '1'
+			sum += pow;
+
+		pow *= 2; // update power of 2
+		len--;
 	}
 
-	return (decimal_num);
-
+	return (sum);
 }
